69ea3843e6f54e64ff6e16787329ed7e15890aac
VALEH <valokech@gmail.com>
2024-02-03 08:01:17 +0300

    Update perform_test.php

---
+++ application/perform_test.php
@@ -0,0 +1,33 @@
+ 
+<?php
+
+include "pathologist_header.php";                 
+?>
+
+
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Perform Test</h1> 
+          </div><!-- /.col -->
+          
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+
+
+
+
+
+
+<?php
+
+include "footer.php";                 
+?>
